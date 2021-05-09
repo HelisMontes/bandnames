9d299c008e7c0865c80b06c07d4263cff25511be
Helis Montes <helismontes@gmail.com>
2021-05-09 01:28:42 -0500

    Crear aplicación de React y los primeros componentes

---
+++ bandnames/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BandAdd } from './componets/BandAdd';
+import { BandList } from './componets/BandList';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="alert">
+        <p>
+          Service status:
+          <span className="text-success">Online</span>
+          <span className="text-danger">Offline</span>
+        </p>
+      </div>
+      <h1>BandNames</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          BandList
+          <BandList />
+        </div>
+        <div className="col-4">
+          BandAdd
+          <BandAdd />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ bandnames/src/componets/BandAdd.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const BandAdd = () => {
+  return (
+    <>
+      <h3>Agregar Band</h3>
+      <form>
+        <input
+          className="form-control"
+          placeholder="Nombre de la banda"
+        />
+      </form>
+    </>
+  )
+}
